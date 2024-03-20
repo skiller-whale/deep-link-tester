ec13e8ccd8df08cc9dc047f0940acbe44054f9bc
Amyas Merivale <merivale@gmail.com>
2024-03-20 15:12:57 +0100

    Make more mobile friendly design

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const scheme = document.querySelector("[name='scheme']");
-const ip = document.querySelector("[name='ip']");
+const address = document.querySelector("[name='address']");
 const path = document.querySelector("[name='path']");
 const link = document.querySelector("#link");
 
 const updateLink = () => {
-  link.href = `${scheme.value}://${ip.value}/--/${path.value}`;
+  link.href = `${scheme.value}://${address.value}${path.value}`;
 };
 
 scheme.addEventListener("input", updateLink);
-ip.addEventListener("input", updateLink);
+address.addEventListener("input", updateLink);
 path.addEventListener("input", updateLink);
 
 updateLink();
